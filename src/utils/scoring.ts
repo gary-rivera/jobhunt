5b72cdc1cdd8faa60edea0dd7157520cddc15df4
gary-rivera <a.gary.rivera@gmail.com>
2025-09-23 14:13:57 -0700

    Simplify cosineSimilarity vector type and add log
    Removed the NomicEmbeddingVector type and updated cosineSimilarity to accept number[] arrays. Added a success log after calculation.

--- src/utils/scoring.ts
+++ src/utils/scoring.ts
@@ -1,12 +1,10 @@
-type NomicEmbeddingVector = readonly number[] & { length: 768 };
-
-function cosineSimilarity(vectorA: NomicEmbeddingVector, vectorB: NomicEmbeddingVector): number {
+function cosineSimilarity(vectorA: number[], vectorB: number[]): number {
   log.info('[cosineSimilarity] Calculating cosine similarity between two vectors');
   const dotProduct = vectorA.reduce((sum, a, i) => sum + a * vectorB[i], 0);
 
   const magnitudeA: number = Math.sqrt(vectorA.reduce((sum, a) => sum + a * a, 0));
   const magnitudeB: number = Math.sqrt(vectorB.reduce((sum, b) => sum + b * b, 0));
-
+  log.success('[cosineSimilarity]');
   return dotProduct / (magnitudeA * magnitudeB);
 }
 
